48d86b9c3a091e25dcef4a402b54779745b728eb
treeder <treeder@gmail.com>
2011-02-19 12:33:18 -0800

    Removed client stuff.

--- lib/appoxy_api.rb
+++ lib/appoxy_api.rb
@@ -2,9 +2,7 @@ require 'active_support/core_ext'
 require 'digest/hmac'
 require 'net/http'
 require 'base64'
-  
+
 require_relative "api/api_controller"
-require_relative "api/client_helper"
 require_relative "api/signatures"
-require_relative "api/client"
 
